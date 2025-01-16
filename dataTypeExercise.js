d784e9410913804b3b13fb2f8f7be0f8a7e77f2b
Freddy PC <freddythepc@gmail.com>
2025-01-15 20:08:30 -0500

    Update: Data Type practice

--- dataTypeExercise.js
+++ dataTypeExercise.js
@@ -1,47 +1,47 @@
 // 1. String - Text data
-let myString = "____"; 
-// console.log("String:", myString);
+let myString = "Hellow World";
+let stringExample = "12345";
+console.log("String:", myString);
+console.log("String:", stringExample);
 
 // 2. Number - Numeric data
-let myNumber = ____; 
-// console.log("Number:", myNumber);
+let myNumber = 12345;
+console.log("Number:", myNumber);
 
 // 3. Boolean - True or False
-let myBoolean = ____; 
-// console.log("Boolean:", myBoolean);
+let myBoolean = true;
+console.log("Boolean:", myBoolean);
 
 // 4. Undefined - Variable declared but not assigned
-let myUndefined = ____; 
-// console.log("Undefined:", myUndefined);
+let myUndefined = undefined;
+console.log("Undefined:", myUndefined);
 
 // 5. Null - Intentional absence of value
-let myNull = ____; 
-// console.log("Null:", myNull);
+let myNull = null;
+console.log("Null:", myNull);
 
 // 6. Symbol - Unique and immutable identifier
-let mySymbol = Symbol("____"); 
-// console.log("Symbol:", mySymbol);
+let mySymbol = Symbol("Fancy Identifier");
+console.log("Symbol:", mySymbol);
 
 // 7. BigInt - Numbers larger than the Number type can handle
-let myBigInt = ____n; 
+let myBigInt = 12345678910n;
 // console.log("BigInt:", myBigInt);
 
-
-
 // 1. Object - Collection of key-value pairs
-let myObject = { name: "____", age: ____ }; 
-// console.log("Object:", myObject);
+let myObject = { name: "John Doe", age: 100 };
+console.log("Object:", myObject);
 
 // 2. Array - Ordered list of values
-// let myArray = [____, ____, ____]; 
-// console.log("Array:", myArray);
+let myArray = [100, 1000, 10000];
+console.log("Array:", myArray);
 
 // 3. Function - Block of reusable code
 // function myFunction() {
-    // console.log("Hello, I am a ____!");
+// console.log("Hello, I am a ____!");
 // }
 // myFunction();
 
 // 4. Date - Represents dates and times
-// let myDate = new Date(); 
+// let myDate = new Date();
 // console.log("Date:", myDate);
