5d54df8644878df94fe98d6317ae2cfd3ec60273
Freddy PC <freddythepc@gmail.com>
2025-01-15 19:19:15 -0500

    Initial commit from yesterday

---
+++ dataTypeExercise.js
@@ -0,0 +1,47 @@
+// 1. String - Text data
+let myString = "____"; 
+// console.log("String:", myString);
+
+// 2. Number - Numeric data
+let myNumber = ____; 
+// console.log("Number:", myNumber);
+
+// 3. Boolean - True or False
+let myBoolean = ____; 
+// console.log("Boolean:", myBoolean);
+
+// 4. Undefined - Variable declared but not assigned
+let myUndefined = ____; 
+// console.log("Undefined:", myUndefined);
+
+// 5. Null - Intentional absence of value
+let myNull = ____; 
+// console.log("Null:", myNull);
+
+// 6. Symbol - Unique and immutable identifier
+let mySymbol = Symbol("____"); 
+// console.log("Symbol:", mySymbol);
+
+// 7. BigInt - Numbers larger than the Number type can handle
+let myBigInt = ____n; 
+// console.log("BigInt:", myBigInt);
+
+
+
+// 1. Object - Collection of key-value pairs
+let myObject = { name: "____", age: ____ }; 
+// console.log("Object:", myObject);
+
+// 2. Array - Ordered list of values
+// let myArray = [____, ____, ____]; 
+// console.log("Array:", myArray);
+
+// 3. Function - Block of reusable code
+// function myFunction() {
+    // console.log("Hello, I am a ____!");
+// }
+// myFunction();
+
+// 4. Date - Represents dates and times
+// let myDate = new Date(); 
+// console.log("Date:", myDate);
---
+++ myFirstFolder/newJavascript.js
@@ -0,0 +1,9 @@
+console.log("Run in terminal!");
+console.log("Run this also!");
+
+let myArray = [1, 2, 3, 4, 5];
+
+const error = undefined;
+console.log(error);
+
+let number = 12345n;
