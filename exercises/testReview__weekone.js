b6d01ebcab69f575c0b3e1813dcb520c7cb9408c
Freddy PC <freddythepc@gmail.com>
2025-01-21 19:42:27 -0500

    update: review and change notes for week one

--- exercises/testReview__weekone.js
+++ exercises/testReview__weekone.js
@@ -1,40 +1,45 @@
 // What is the significance of JavaScript in modern web development,
 /* JavaScript in modern development is significant 
-   because it allows users to interact with the webpage. */
+   because it allows users to interact with the webpage. 
+   Its a dynamic programming language.
+   Client-side web Development!*/
 
 //List and explain all JavaScript primitive data types, including their typical use cases.
-/*  String = text field for user inputs
-    Number = 
-    BigiNT = 
-    Boolean = 
-    Null = 
-    Undefined = 
-    Symbol = 
+/*  String = textual data > display info on the client side / store data (names and emails in backend)
+    Number = numerical data > use values to do calculations
+    BigiNT = large integers > it can ONLY be used with other bigInt values
+    Boolean = true or false stataments > set logical conditions, user or coder accessibility 
+    Null = intentional absence of value > letting user leave user inputs as empty
+    Undefined = uninitialized variable or non-intential absence of value
+    Symbol = unique and immutable property keys
 */
 
 //Describe how 'undefined' is different from 'null' in JavaScript, with examples of when each might be encountered
-/*  A Undefined data type means it is decalred but there is a absence of a value. 
+/*  A Undefined data type means it is declared but there is a absence of a value. 
     If a input field requires an input, 
-    a undefined value would let the JavaScript engine know that the field is reqired
+    a undefined value would let the JavaScript engine know that the field is required
     then let the user know. 
 
     A null data type is an intentional absence of a value. 
-    If a user input isn't filled, this data type doesn't result in an error. 
+    If a user input isn't filled, this data type doesn't result in an error on the console. 
 
 */
 
 //What is the difference between the terms 'undefined' and 'undeclared' in JavaScript?
-/* */
+/* Undefined means that it has been declared but not assigned a value.
+   Undeclared means it has not been declared at all; does NOT exist in the current scope. */
 
 //How does JavaScript differentiate between 'null' and an empty object
-/* */
+/* A null value means that the value was intentionally absent. 
+   A empty object DOES NOT conactin any properties or methods.
+    */
 
 //What are the limitations of the BigInt data type,
 // and how does it integrate with other number types in JavaScript?
-/* */
+/* You CANNOT mix BigInt values with any other data type.*/
 
 //Explain the purpose of the <script> tag  in HTML
-/* The Script tag connects the JavaScript file to a HTML file.*/
+/*  The Script tag connects the JavaScript file to a HTML file. */
 
 //Write a JavaScript statement to display "Hello, how are you?" in the console
 /*  Let greeting = "Hello, how are you?";
@@ -42,9 +47,10 @@
     */
 
 //What are the two types of comments in JavaScript, and how do they impact code readability and execution?
-/* */
+/* The double slash and the slash and star 
+    The slash and star lets you organize commenst that are longer than one-line!*/
 
 //What is the result of '10' - '5' + 5 in JavaScript, and why?
 /*  10
     The JavaScript engine reads the number in parenthesis as a number
-    , known as String Coersion. */
+    , known as String Coersion. It is also possible with String Concatenation!*/
