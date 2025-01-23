48b3f258fe4107a2cd761d424c53d1b7905798ca
Freddy PC <freddythepc@gmail.com>
2025-01-22 21:45:28 -0500

    update: connect html to js file and test code

--- myFirstFolder/javascript.js
+++ myFirstFolder/javascript.js
@@ -1,17 +1,68 @@
-// // You cannot say if "false" email
-// // if there is no email (NOT! Negation)
-// if (!Object.email) {
+// Ternary
+const score = 85;
+const grade = score >= 90 ? "A" : score >= 80 ? "B" : "C";
+
+console.log(grade); // Output: "B"
+
+// switch statement >
+let setColor = "red";
+
+switch (setColor) {
+  case "red":
+    console.log("Stop!");
+    break;
+  case "yellow":
+    console.log("Caution");
+    break;
+  case "green":
+    console.log("GO!");
+    break;
+  default:
+    console.log("Your input is invalid.");
+}
+
+// switch statament >
+let setSeason = "June";
+switch (setSeason) {
+  case "December":
+  case "January":
+  case "February":
+    console.log("Winter is coming.");
+    break;
+  case "March":
+  case "April":
+  case "May":
+    console.log("Spring time.");
+    break;
+  case "June":
+  case "July":
+  case "August":
+    console.log("When I metcha in the Summer.");
+    break;
+  case "September":
+  case "October":
+  case "November":
+    console.log("Fall time");
+    break;
+  default:
+    console.log("Invalid month name input!");
+}
+
+// Terenary program >
+// If number has no remaidner return even : if there is a remainder return false
+let number = 115;
+let evenNumber = number % 2 == 0 ? `${number} is even` : `${number} is odd`;
+
+console.log(evenNumber);
+
+// // function syntax
+// function (parameter) {
+//     // code that will be executed
 // }
+// functionName(); // call function (with or wirhout arguements)
 
-// AND > conjunction > both true = true
-// OR > alternative > one true = true
-// NOT > negation = the opposite
-
-//console.log(a && b && c || d);
-//false && true && (false or true)
-
-// value of 0 = false
-// Positive value = true
-
-// 0 (its a fals value but it prints zero)
-// "" (empty string evaluates to false)
+// function example
+function greetName(name) {
+  console.log("Hello " + name + "!");
+}
+greetName("Martin");
