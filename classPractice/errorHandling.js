15e12387720edccefecc30ebce92c243155e11b2
Freddy PC <freddythepc@gmail.com>
2025-01-30 21:36:52 -0500

    refactor: initial commit

---
+++ classPractice/errorHandling.js
@@ -0,0 +1,10 @@
+//catch this error console.log("Start);
+console.log("Hello, world!");
+console.log("End");
+
+//catch this error
+let myArray = new Array(-5);
+
+// create a finally block
+let a = 10;
+let b = c;
