f29e5082097737165bbf3c0edc4afc9a79f829ed
Freddy PC <freddythepc@gmail.com>
2025-01-31 20:47:04 -0500

    refactor: initial commit for week 3 review

--- review/review13.js
+++ review/review13.js
@@ -1,103 +1,103 @@
-////1 Explain the difference between Object.keys() , Object.values() ,
-// and Object.entries() . Provide examples.
+// ////1 Explain the difference between Object.keys() , Object.values() ,
+// // and Object.entries() . Provide examples.
 
-// Object.keys() -> The Object keys methods returns an array of all the keys in the object.
-// Object.values() -> The Object values methods returns an array of all the values in the object.
-/* Object.entries() -> The Object entries method returns an array of all the key-value pairs in a object, 
-                        where each pair is an array [key, value]. */
-let fruits = {
-  name: "apple",
-  food: "fruit",
-  macro: "carb",
-};
-console.log(`Keys from the object: ${Object.keys(fruits)}`);
-console.log(`Values from the object: ${Object.values(fruits)}`);
-console.log(`Key-Value pairs from the object: ${Object.entries(fruits)}`);
-
-////2 How do for...of and for...in loops differ in JavaScript? Provide
-// scenarios where each is preferable
-
-// for-of loops iterate over objects like arrays, strings, maps, and sets.
-// They are best used for arrays!
-
-// for-in loops iterate over properties in a object.
-// They are best used for objects!
-
-//3 How do you use the length property of an array to iterate over it//////////////////
-// dynamically? (In for loops)
-
-// You can refer i (in a for loop) equal to the array length.
-//  (i < Array.length) -> every array element
-
-////4 Write a for loop that iterates backward through an array and
-// prints its elements to the console.
-
-let array = [2, 4, 6, 8, 10];
-for (i = array.length - 1; i >= 0; i--) {
-  // console.log(i); // returns a number value of array.length = 5
-  console.log(array[i]);
-  // array.length === 5
-  // The arrays last index is ACTUALLY 4
-  // array.length - 1 === 4
-}
-
-// the start point is the array.length = 5 (total array values)
-// if i is greater than or equal to 1 -> go to the for loop body
-// i-- -> decrement (going backwards from the array value "5")
-
-////5 Which loops in JavaScript are best suited for iterating over objects,
-// and why?
-
-// The for-in loop because it iterates over properties in a object.
-
-////6 Write a function that demonstrates the difference between
-// function declarations and function expressions in JavaScript.
-
-// Function declarations define a function using the function keyword at the beginning of the statement.
-// They are hoisted ===
-// We use return so the function gives back a value that can be used.
-// Functions do not automatically return values unless you tell them...
-function subtration(a, b) {
-  return a - b;
-}
-console.log(subtration(5, 1));
-
-// Funtion expressions defines a function within an expression.
-const addition = function (a, b) {
-  return a + b;
-};
-console.log(addition(5, 1));
-
-//7 Create an object named people where each key is a person's name and
-// the value is an array of their favorite foods.
-//Use for...in to iterate over the object keys (names).
-// Inside that loop, use for...of to print each person’s favorite foods.
-
-// let people = {
-//   bob: ["hamburger", "fries"],
-//   maria: ["enchiladas", "tacos"],
-//   frank: ["spagetti", "fettechini"],
-//   liz: ["ramen", "sushi"],
+// // Object.keys() -> The Object keys methods returns an array of all the keys in the object.
+// // Object.values() -> The Object values methods returns an array of all the values in the object.
+// /* Object.entries() -> The Object entries method returns an array of all the key-value pairs in a object,
+//                         where each pair is an array [key, value]. */
+// let fruits = {
+//   name: "apple",
+//   food: "fruit",
+//   macro: "carb",
 // };
+// console.log(`Keys from the object: ${Object.keys(fruits)}`);
+// console.log(`Values from the object: ${Object.values(fruits)}`);
+// console.log(`Key-Value pairs from the object: ${Object.entries(fruits)}`);
 
-// // Nested loop..........
-// for (let key in people) {
-//   // people === keys > bob
-//   console.log("\n" + key); //
-//   for (let munch of people[key]) {
-//     // people[eat] === values > hamburger, fries
-//     console.log(munch);
-//   }
+// ////2 How do for...of and for...in loops differ in JavaScript? Provide
+// // scenarios where each is preferable
+
+// // for-of loops iterate over objects like arrays, strings, maps, and sets.
+// // They are best used for arrays!
+
+// // for-in loops iterate over properties in a object.
+// // They are best used for objects!
+
+// //3 How do you use the length property of an array to iterate over it//////////////////
+// // dynamically? (In for loops)
+
+// // You can refer i (in a for loop) equal to the array length.
+// //  (i < Array.length) -> every array element
+
+// ////4 Write a for loop that iterates backward through an array and
+// // prints its elements to the console.
+
+// let array = [2, 4, 6, 8, 10];
+// for (i = array.length - 1; i >= 0; i--) {
+//   // console.log(i); // returns a number value of array.length = 5
+//   console.log(array[i]);
+//   // array.length === 5
+//   // The arrays last index is ACTUALLY 4
+//   // array.length - 1 === 4
 // }
 
-// Example from above
-try {
-  for (let key in people) {
-    console.log("\n" + key); //
-    for (let munch of people[key]) {
-      console.log(munch);
-    }
-  }
-} catch (error) {
-  console.log("You have an error: " + error);
-}
+// // the start point is the array.length = 5 (total array values)
+// // if i is greater than or equal to 1 -> go to the for loop body
+// // i-- -> decrement (going backwards from the array value "5")
+
+// ////5 Which loops in JavaScript are best suited for iterating over objects,
+// // and why?
+
+// // The for-in loop because it iterates over properties in a object.
+
+// ////6 Write a function that demonstrates the difference between
+// // function declarations and function expressions in JavaScript.
+
+// // Function declarations define a function using the function keyword at the beginning of the statement.
+// // They are hoisted ===
+// // We use return so the function gives back a value that can be used.
+// // Functions do not automatically return values unless you tell them...
+// function subtration(a, b) {
+//   return a - b;
+// }
+// console.log(subtration(5, 1));
+
+// // Funtion expressions defines a function within an expression.
+// const addition = function (a, b) {
+//   return a + b;
+// };
+// console.log(addition(5, 1));
+
+// //7 Create an object named people where each key is a person's name and
+// // the value is an array of their favorite foods.
+// //Use for...in to iterate over the object keys (names).
+// // Inside that loop, use for...of to print each person’s favorite foods.
+
+// // let people = {
+// //   bob: ["hamburger", "fries"],
+// //   maria: ["enchiladas", "tacos"],
+// //   frank: ["spagetti", "fettechini"],
+// //   liz: ["ramen", "sushi"],
+// // };
+
+// // // Nested loop..........
+// // for (let key in people) {
+// //   // people === keys > bob
+// //   console.log("\n" + key); //
+// //   for (let munch of people[key]) {
+// //     // people[eat] === values > hamburger, fries
+// //     console.log(munch);
+// //   }
+// // }
+
+// // Example from above
+// try {
+//   for (let key in people) {
+//     console.log("\n" + key); //
+//     for (let munch of people[key]) {
+//       console.log(munch);
+//     }
+//   }
+// } catch (error) {
+//   console.log("You have an error: " + error);
+// }
---
+++ review/review__week3.js
@@ -0,0 +1,122 @@
+/************************CORE CONCEPT PRACTICE*************************************
+FOCUS: VARIABLES, CONDITIOONALS, LOOPS, FUNCTIONS, ARRAYS, AND OBJECTS
+
+/* 1
+Declare a let variable called greeting and assign the value "Good Morning".
+Create a const variable named hour with a value of 9.
+Reassign greeting to "Good Afternoon" if the hour is greater than or equal to 12.*/
+
+/* 2
+Write a program that checks a number stored in the variable temperature.
+-If the number is below 0, print "It's freezing!".
+-If it’s between 0 and 30, print "It's cold.".
+-If it’s above 30, print "It's warm."*/
+
+/* 3
+Create a for loop that prints the numbers 1 to 10.
+Add a while loop that sums numbers from 1 to 5 and prints the result.*/
+
+/* 4
+Write a function named multiply that takes two parameters and returns their product.
+Call the function with values 4 and 5 and print the result.
+Try changing the inputs to negative numbers.*/
+
+/* 5
+Create an array of numbers: [3, 6, 9, 12].
+Use a loop to print each number multiplied by 2.
+Create an object named car with properties make, model, and year. Print the object.*/
+
+/* 6
+Declare a let variable isLoggedIn and set it to false.
+Change it to true if the username variable equals "admin".*/
+
+/* 7
+Create a variable score.
+If score is 90 or above, print "A".
+If between 80 and 89, print "B".
+If between 70 and 79, print "C".
+Otherwise, print "F".*/
+
+/* 8
+Write a for loop to print only even numbers from 1 to 20.
+Create a while loop that multiplies numbers from 1 to 5 and prints the result.*/
+
+/* 9
+Write a function greetUser that takes a name and prints "Hello, [name]!".
+Call it with different names like "Alice" and "Bob".*/
+
+/* 10
+Create an array of strings: ["apple", "banana", "cherry"].
+Loop through and print each item in uppercase. -> there is a method for this
+Create an object student with name, age, and grade properties. Print each property.*/
+
+/* 11
+Declare a variable totalAmount and set it to 100.
+Create a constant discountRate set to 0.2.
+Calculate the final price after applying the discount and store it in a variable finalPrice.*/
+
+/* 12
+Create a program that checks a number stored in a variable age:
+-If the age is less than 18, print "You are a minor.".
+-If it’s between 18 and 64, print "You are an adult."
+-Otherwise, print "You are a senior citizen."*/
+
+/* 13
+Create a for loop to print the numbers from 10 down to 1 in reverse.
+Use a while loop to print the first 5 multiples of 3.*/
+
+/* 14
+Write a function isEven that takes a number and returns true if the number is even, otherwise false. Test the function with values like 10, 15, and 22.*/
+
+/* 15
+Create an array of 5 numbers: [10, 20, 30, 40, 50].
+Write a loop to calculate the sum of the numbers.
+Create an object book with properties title, author, and pages. Add a new property genre and print the updated object.*/
+
+/* 16
+Declare variables for firstName and lastName and assign them your name.
+Create a fullName variable that combines firstName and lastName.
+Print the result using template literals.*/
+
+/* 17
+Declare a variable score and assign it a number between 0 and 100.
+Print:
+"Pass" if score is 50 or higher.
+"Fail" if score is below 50.*/
+
+/* 18
+Create a for loop that prints the squares of numbers from 1 to 10.
+Write a while loop to print numbers from 1 to 5 in reverse.*/
+
+/* 19
+Write a function greet that takes a name and a timeOfDay (morning, afternoon, or evening).
+Print a greeting like "Good morning, John!".*/
+
+/* 20
+-Create an array of fruits: ["apple", "orange", "grape", "banana"].
+-Use a loop to print only fruits with more than 5 letters.
+
+
+
+
+/* 21
+Declare a variable length set to 10 and a variable width set to 5.
+Calculate the area of a rectangle using these variables and print the result.*/
+
+/* 22
+Write a program to check if a number stored in num is divisible by 3, 5, or both:
+If divisible by 3, print "Fizz".
+If divisible by 5, print "Buzz".
+If divisible by both, print "FizzBuzz".
+If not divisible by 3 or 5, print the input
+If not a number, print 'Not a number'*/
+
+/* 23
+Write a loop to print the factorial of a number (e.g., 5! = 120).
+Use a loop to reverse the characters of a string "hello" and print it.*/
+
+/* 24
+Write a function sumArray that takes an array of numbers and returns the sum of all elements.*/
+
+/* 25
+Create an object pet with properties type, name, and age. Add a method to print a sentence like: "My dog's name is Max, and he is 4 years old."*/
